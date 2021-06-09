9f6d3f4c0376a3e8f8811f59c17f8645281a39f9
Àlex Figueroa Boronat <alfil229912@gmail.com>
2021-06-09 22:52:23 +0200

    Fichero Server para añadir Workers
    Según la opción escogida tratará los workers.

--- Server.py
+++ Server.py
@@ -3,7 +3,10 @@ import WordCount_pb2_grpc as archivoServer
 import ArchivoContador as archivoPython
 import grpc
 from concurrent import futures
+from multiprocessing import Process
 
+WORKERS = []
+WORKERS_ID = 0
 class WordCount(archivoServer.WordCountServicer):
     def elContador(self, request, context):
         option = request.option
@@ -12,14 +15,41 @@ class WordCount(archivoServer.WordCountServicer):
         if 0 < option < 3:
             contenido = archivoPython.WordCount(files, option)
             return archivoClient.fileData(fileData=contenido)
+	if option == 3:
+		return archivoClient.fileData(fileData=showWorkers)
+	if option == 4:
+		createWorker()
+		return archivoClient.fileData(fileData="Worker creado con exito")
+	if option == 5:
+		deleteWorker(idWorker)
+		return archivoClient.fileData(fileData="Worker eliminado correctamente")
 
 def StartServer():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    createWorker()
     archivoServer.add_WordCountServicer_to_server(WordCount(), server)
     server.add_insecure_port('[::]:50051')
     print("Servidor iniciado")
     server.start()
     server.wait_for_termination()
+
+def createWorker():
+    global WORKERS
+    global WORKERS_ID
+    proc = Process(target=startWorker, args=(WORKERS_ID,))
+    proc.start()
+    WORKERS.append(WORKERS_ID)
+    print(WORKERS[WORKERS_ID])
+    WORKERS_ID += 1
+
+def startWorker():
+    ...
+
+def showWorkers():
+	wActivos = ""
+	for i in range (len(WORKERS)):
+		wActivos += "Worker con ID --> " + WORKERS[i] + "esta activo" + "\n"
+	return wActivos
 
 if __name__ == '__main__':
     StartServer()
