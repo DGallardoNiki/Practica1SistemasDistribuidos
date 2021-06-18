d6779b21c17af9b709d3503f8c15f478e355df68
JuanCigales <cigales.juan.mrm@gmail.com>
2021-06-19 01:07:30 +0200

    Modificaciones del server
    Se han acabado de incorporar los servicios del archivo proto y se ha añadido la lógica que seguirá cada worker (faltan leves cambios de optimización)
    
    Co-Authored-By: DGallardoNiki <69145556+DGallardoNiki@users.noreply.github.com>
    Co-Authored-By: alexfigue <69157035+alexfigue@users.noreply.github.com>

--- Server.py
+++ Server.py
@@ -42,73 +42,49 @@ class WordCount(archivoServer.WordCountServicer):
             createWorker()
         return archivoStub.returnsString(cadena=str(nWorkers))
 
-    def reponseData (self, request, context):
-        parar = "1"
-        #print(f"-----> Ficheros {ficherosRedis.llen('Fichero')}")
-        #print(f"-----> Resultados {ficherosRedis.llen('Resultados')}")
-        if ficherosRedis.llen('Fichero') == 0 and ficherosRedis.llen('Resultados') == (int(request.fileData) + 1):
-            parar = "0"
-            print("Hey guapo desde el server")
-        return archivoClient.fileData(fileData=parar)
-
-    # Add nombre de los ficheros a la cola del redis
-    def crearContenido(self, request, context):
-        global ficherosRedis
-        option = request.option
-        ficherosRedis.flushall()
-        files = request.files
-        files = files.split(" ")
-        i = 0
-        cantidad = len(files)
-        while i < cantidad:
-            if files[i] != "None" and files[i] != "NoneType":
-                if option == 1:
-                    mensaje = "CW "+files[i]
-                    ficherosRedis.rpush('Fichero', mensaje)
-                if option == 2:
-                    mensaje = "WC "+files[i]
-                    ficherosRedis.rpush('Fichero', mensaje)
-                i += 1
-        return archivoClient.fileData(fileData=str(cantidad))
-
-    def crearWorkers(self, request, context):
-        idWorker = request.nWorkers
-        i = 0
-        for i in range(i, idWorker):
-                crearWorker()
-        return archivoClient.fileData(fileData="He creado los workers que se me han pedido, jeje xD")
-
-    def elContador(self, request, context):
-        global option
-        option = request.option
-        idWorker = request.idWorker
-        i=0         
+    def popResponse(self, request, context):
+        global colaRedis
+        auxString = ""
+        print(colaRedis.llen('keyResultado'))
+        while colaRedis.llen('keyResultado') > 0:
+            print(f"Antes del pop --> {colaRedis.lrange('keyResultado', 0, -1)}")
+            auxResultado = colaRedis.lpop('keyResultado')           
+            if auxResultado != None and auxResultado != "":
+                auxResultado = auxResultado.decode("utf-8")
+                auxString += auxResultado + " "
+        if auxString != "":
+           auxString = auxString[:-1]
+        print(f"Despues del pop --> {colaRedis.lrange('keyResultado', 0, -1)}")
+        colaRedis.flushall()
+        colaRedis.flushdb()
+        return archivoStub.returnsString(cadena=auxString)
+    # Servicio que se encarga de realizar las opciones 3, 4 y 5 que pueda pedir realizar
+    #   el cliente. Se retornará el resultado generado
+    def someOpc(self, request, context):
+        option = request.opc
+        id = request.idW
+        auxString = ""
         if option == 3:
-            contenido = showWorkers()
-            return archivoClient.fileData(fileData=contenido)
-        else:
-            return archivoClient.fileData(fileData="Nada")
+            auxString = showWorkers()
+        if option == 4:
+            auxString = createWorker()
+        if option == 5:
+            auxString = killWorker(id)
+        return archivoStub.returnsString(cadena=auxString)
 
-    def response(self, request, context):
-        global ficherosRedis
-        resultados = ""
-        cantidad = int(request.fileData)
-        print(f"Me llega un total de --> {cantidad}")
-        valores = False
-        i = 0
-        while ficherosRedis.llen('Resultados') > 0 and ficherosRedis.llen('Fichero') == 0 and i < cantidad:
-            print("Hola, hay resultados y no hay tareas")
-            valores = True
-            resultado = ficherosRedis.lpop('Resultados')
-            if resultado != None and resultado != "":
-                resultado = resultado.decode("utf-8")
-                resultados += resultado + " "
-            i += 1
-        if valores:
-            resultados = resultados[:-1]
-        return archivoClient.fileData(fileData=resultados)
-
-
+    #Servicio que permite averiguar como se encuentras las diferentes colas
+    def checkQueue(self, request, context):
+        act = "-1"
+        if colaRedis.llen('keyFichero') > 0 and colaRedis.llen('keyResultado') > 0:
+            act = "0"
+        if colaRedis.llen('keyFichero') == 0 and colaRedis.llen('keyResultado') > 0:
+            act = "1"
+        if colaRedis.llen('keyFichero') > 0 and colaRedis.llen('keyResultado') == 0:
+            act = "2"
+        if colaRedis.llen('keyFichero') == 0 and colaRedis.llen('keyResultado') == 0:
+            act = "3"
+        return archivoStub.returnsString(cadena=act)
+           
 
 def StartServer():
     global ficherosRedis
@@ -120,60 +96,64 @@ def StartServer():
     print("Servidor iniciado")
     server.start()
     server.wait_for_termination()
-Workers = []
-idWorker = 0
-def crearWorker():
-    global Worker
+
+def createWorker():
+    global listaWorkers
     global idWorker
-    procesoWorker = Process(target=iniciarWorker, args=(idWorker, ))
-    procesoWorker.start()
-    Workers.append(procesoWorker)
+    aux = ""
+    worker = Process(target=workerLogic)
+    worker.start()
+    listaWorkers.append(worker)
     idWorker += 1
 
-def iniciarWorker(idWorker):
-    global responseContenido
-    global resultadoRedis
+def killWorker(idW):
+    global idWorker
+    global listaWorkers
+    for i in range(len(listaWorkers)):
+        if (listaWorkers[i]):
+            print(listaWorkers[i])
+
+def workerLogic():
+    global colaRedis
     option = 0
     suma = 0
-    i = 0
     stop = False
+    cadena = 0
+    i = 0
     while True:
-        i = 0
-        #print("Entro cola Ficheros")
-        if ficherosRedis.llen('Fichero') > 0:
-            nombreFichero = (ficherosRedis.lpop('Fichero'))#.decode("utf-8")
-            if nombreFichero != None and nombreFichero != "":
-                nombreFichero = (nombreFichero.decode("utf-8")).split(" ")
-                if nombreFichero[0] == "WC":
+        if colaRedis.llen('keyFichero') > 0:
+            i = 0
+            stop = False
+            auxNombre = colaRedis.lpop('keyFichero')           
+            if auxNombre != None and auxNombre != "":
+                auxNombre = (auxNombre.decode("utf-8")).split(" ")
+                cantidad = int(auxNombre[2])
+                if auxNombre[0] == "WC":
                     option = 2
                 else:
                     option = 1
-                
-                responseContenido = archivoPython.WordCount(nombreFichero[1], option)
-                ficherosRedis.rpush('Resultados', responseContenido)
-        ficherosRedis.rpush('Resultados', -1)
-        if ficherosRedis.llen('Fichero') == 0 and option == 1 and ficherosRedis.llen('Resultados') > 0:
-            lista = ""
-            cadena = 0
-            while stop != True:
-                lista = ficherosRedis.lrange('Resultados', 0, -1)
-                bytesObj = lista[i]
-                cadena = int(bytesObj.decode("utf-8"))
-                if cadena == -1:
-                    stop = True
-                else: 
+                datosAnalisis = contador.WordCount(auxNombre[1], option)
+                colaRedis.rpush('keyResultado', datosAnalisis)
+        if colaRedis.llen('keyFichero') == 0 and option == 1 and colaRedis.llen('keyResultado') == cantidad and stop != True and cantidad > 1:#and cadena != -1 
+            stop = True
+            lista = colaRedis.lrange('keyResultado', 0, -1)
+            i=0
+            while i<cantidad:
+                objBytes = lista[i]
+                if objBytes != None and objBytes != "":
+                    cadena = int(objBytes.decode("utf-8"))
                     suma += cadena
-                    i += 1
-                    print(f"----> {type(cadena)} +++++> {suma}")
-                print(i)
-            #print("No bucle infinito")
-            ficherosRedis.rpush('Resultados', suma)
-
+                i += 1
+            if int((colaRedis.lindex('keyResultado', -1).decode("utf-8"))) != suma:
+                colaRedis.rpush('keyResultado', suma)
+                suma=0
+                cadena=0
+        option = 0
 
 def showWorkers():
     wActivos = ""
-    for i in range(len(Workers)):
-        wActivos += "Worker --> " + str(Workers[i]) + "\n"
+    for i in range(len(listaWorkers)):
+        wActivos += "Worker --> " + str(listaWorkers[i]) + "\n"
     return wActivos
 
 
